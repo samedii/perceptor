e69c761db866a73adf8c4748c3f0a482fb24bfc4
Richard Löwenström <samedii@gmail.com>
2022-04-05 23:35:46 +0200

    fix: incorrect name in diffusion import

--- perceptor/transforms/__init__.py
+++ perceptor/transforms/__init__.py
@@ -1,4 +1,4 @@
 from .clamp_with_grad import ClampWithGrad, clamp_with_grad
-from .diffusion import Diffusion, BrushDiffusion
+from .diffusion import Diffusion, BruteDiffusion
 from .resize import resize
 from .super_resolution import SuperResolution
