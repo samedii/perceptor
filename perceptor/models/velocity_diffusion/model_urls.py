bdc7bc332a54892f064acbbc34a81460406fda25
Richard Löwenström <samedii@gmail.com>
2022-04-16 00:58:03 +0200

    fix: velocity diffusion weights moved

--- perceptor/models/velocity_diffusion/model_urls.py
+++ perceptor/models/velocity_diffusion/model_urls.py
@@ -1,7 +1,7 @@
 model_urls = {
-    "yfcc_2": "https://v-diffusion.s3.us-west-2.amazonaws.com/yfcc_2.pth",
-    "yfcc_1": "https://v-diffusion.s3.us-west-2.amazonaws.com/yfcc_1.pth",
-    "cc12m_1": "https://v-diffusion.s3.us-west-2.amazonaws.com/cc12m_1.pth",
-    "cc12m_1_cfg": "https://v-diffusion.s3.us-west-2.amazonaws.com/cc12m_1_cfg.pth",
-    "wikiart": "https://v-diffusion.s3.us-west-2.amazonaws.com/wikiart_256.pth",
+    "yfcc_2": "https://the-eye.eu/public/AI/models/v-diffusion/yfcc_2.pth",
+    "yfcc_1": "https://the-eye.eu/public/AI/models/v-diffusion/yfcc_1.pth",
+    "cc12m_1": "https://the-eye.eu/public/AI/models/v-diffusion/cc12m_1.pth",
+    "cc12m_1_cfg": "https://the-eye.eu/public/AI/models/v-diffusion/cc12m_1_cfg.pth",
+    "wikiart": "https://the-eye.eu/public/AI/models/v-diffusion/wikiart_256.pth",
 }
