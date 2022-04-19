fix: gradients through finetuned latent diffusion model

perceptor/drawers/diffusion/pseudo_linear_sampler.py
@@ -44,7 +44,6 @@ class PseudoLinearSampler(DrawingInterface):
     def __len__(self):
         return len(self.steps) - 1
 
-    @torch.no_grad()
     def eps_(self, eps):
         self.called_eps += 1
         if len(self.plms_eps_queue) < 3:
@@ -52,7 +51,6 @@ class PseudoLinearSampler(DrawingInterface):
         else:
             return self.plms_eps_(eps)
 
-    @torch.no_grad()
     def plms_eps_(self, eps):
         eps_prime = (
             55 * eps
@@ -61,13 +59,13 @@ class PseudoLinearSampler(DrawingInterface):
             - 9 * self.plms_eps_queue[-3]
         ) / 24
         self.plms_eps_queue = nn.Parameter(
-            torch.cat([self.plms_eps_queue[1:], eps[None]]), requires_grad=False
+            torch.cat([self.plms_eps_queue[1:], eps.detach()[None]]),
+            requires_grad=False,
         )
         return eps_prime
 
-    @torch.no_grad()
     def ddim_eps_(self, eps):
         self.plms_eps_queue = nn.Parameter(
-            torch.cat([self.plms_eps_queue, eps[None]]), requires_grad=False
+            torch.cat([self.plms_eps_queue, eps.detach()[None]]), requires_grad=False
         )
         return eps

perceptor/models/latent_diffusion/finetuned/nn.py
@@ -4,6 +4,7 @@ Various utilities for neural networks.
 
 import math
 
+import torch
 import torch as th
 import torch.nn as nn
 import torch.nn.functional as F
@@ -165,32 +166,55 @@ def checkpoint(func, inputs, params, flag):
         return func(*inputs)
 
 
-class CheckpointFunction(th.autograd.Function):
+class CheckpointFunction(torch.autograd.Function):
     @staticmethod
+    @torch.cuda.amp.custom_fwd
     def forward(ctx, run_function, length, *args):
         ctx.run_function = run_function
-        ctx.input_tensors = list(args[:length])
-        ctx.input_params = list(args[length:])
-        with th.no_grad():
-            output_tensors = ctx.run_function(*ctx.input_tensors)
+        ctx.input_length = length
+        ctx.save_for_backward(*args)
+        with torch.no_grad():
+            output_tensors = ctx.run_function(*args[:length])
         return output_tensors
 
     @staticmethod
+    @torch.cuda.amp.custom_bwd
     def backward(ctx, *output_grads):
-        ctx.input_tensors = [x.detach().requires_grad_(True) for x in ctx.input_tensors]
-        with th.enable_grad():
-            # Fixes a bug where the first op in run_function modifies the
-            # Tensor storage in place, which is not allowed for detach()'d
-            # Tensors.
-            shallow_copies = [x.view_as(x) for x in ctx.input_tensors]
-            output_tensors = ctx.run_function(*shallow_copies)
-        input_grads = th.autograd.grad(
-            output_tensors,
-            ctx.input_tensors + ctx.input_params,
-            output_grads,
-            allow_unused=True,
+        args = list(ctx.saved_tensors)
+
+        # Filter for inputs that require grad. If none, exit early.
+        input_indices = [i for (i, x) in enumerate(args) if x.requires_grad]
+        if not input_indices:
+            return (None, None) + tuple(None for _ in args)
+
+        with torch.enable_grad():
+            for i in input_indices:
+                if i < ctx.input_length:
+                    # Not sure why the OAI code does this little
+                    # dance. It might not be necessary.
+                    args[i] = args[i].detach().requires_grad_()
+                    args[i] = args[i].view_as(args[i])
+            output_tensors = ctx.run_function(*args[: ctx.input_length])
+
+        if isinstance(output_tensors, torch.Tensor):
+            output_tensors = [output_tensors]
+
+        # Filter for outputs that require grad. If none, exit early.
+        out_and_grads = [
+            (o, g) for (o, g) in zip(output_tensors, output_grads) if o.requires_grad
+        ]
+        if not out_and_grads:
+            return (None, None) + tuple(None for _ in args)
+
+        # Compute gradients on the filtered tensors.
+        computed_grads = torch.autograd.grad(
+            [o for (o, g) in out_and_grads],
+            [args[i] for i in input_indices],
+            [g for (o, g) in out_and_grads],
         )
-        del ctx.input_tensors
-        del ctx.input_params
-        del output_tensors
-        return (None, None) + input_grads
+
+        # Reassemble the complete gradient tuple.
+        input_grads = [None for _ in args]
+        for (i, g) in zip(input_indices, computed_grads):
+            input_grads[i] = g
+        return (None, None) + tuple(input_grads)

perceptor/models/velocity_diffusion/velocity_diffusion.py
@@ -28,31 +28,11 @@ class VelocityDiffusion(torch.nn.Module):
         print(f"Loaded checkpoint {checkpoint_path}")
         self.model.eval()
         self.model.requires_grad_(False)
-        self.encodings = None
 
     def to(self, device):
         super().to(device)
         if device == torch.device("cuda"):
             self.model.half()
-        return self
-
-    def add_texts_(self, texts):
-        return self.add_encodings_(
-            models.CLIP(self.model.clip_model).encode_texts(texts)
-        )
-
-    def add_images_(self, images):
-        return self.add_encodings_(
-            models.CLIP(self.model.clip_model).encode_images(images)
-        )
-
-    def add_encodings_(self, encodings):
-        if self.encodings is None:
-            self.encodings = torch.nn.Parameter(encodings, requires_grad=False)
-        else:
-            self.encodings = torch.nn.Parameter(
-                torch.cat([self.encodings, encodings]), requires_grad=False
-            )
         return self
 
     def alphas(self, t):
@@ -67,20 +47,34 @@ class VelocityDiffusion(torch.nn.Module):
         _, sigmas = utils.t_to_alpha_sigma(t)
         return sigmas[:, None, None, None]
 
-    def forward(self, images, t):
+    def forward(self, images, t, conditioning=None):
         if isinstance(t, float) or t.ndim == 0:
             t = torch.full((images.shape[0],), t).to(images)
-        return self.denoise(images, t)
+        return self.denoise(images, t, conditioning=None)
+
+    def conditioning(self, texts=None, images=None, encodings=None):
+        clip_model = models.CLIP(self.model.clip_model)
+
+        all_encodings = list()
+        if texts is not None:
+            all_encodings.append(clip_model.encode_texts(texts))
+        if images is not None:
+            all_encodings.append(clip_model.encode_images(images))
+        if encodings is not None:
+            all_encodings.append(encodings)
+        if len(all_encodings) == 0:
+            raise ValueError("Must provide at least one of texts, images, or encodings")
+        return torch.stack(all_encodings, dim=0).mean(dim=0)[None]
 
     @torch.cuda.amp.autocast()
-    def velocity(self, diffused, t):
+    def velocity(self, diffused, t, conditioning=None):
         x = diffusion_space.encode(diffused)
         if x.shape[1:] != self.model.shape:
             raise ValueError(
                 f"Velocity diffusion model {self.name} only works well with shape {self.model.shape}"
             )
         if hasattr(self.model, "clip_model"):
-            model_fn = partial(self.model, clip_embed=self.encodings.mean(dim=0)[None])
+            model_fn = partial(self.model, clip_embed=conditioning.squeeze(dim=1))
         else:
             model_fn = self.model
 
@@ -100,14 +94,14 @@ class VelocityDiffusion(torch.nn.Module):
             pred * alphas[:, None, None, None] + noise * sigmas[:, None, None, None]
         )
 
-    def denoise(self, diffused, t, eps=None):
+    def denoise(self, diffused, t, conditioning=None, eps=None):
         x = diffusion_space.encode(diffused)
         if isinstance(t, float) or t.ndim == 0:
             t = torch.full((x.shape[0],), t).to(x)
 
         alphas, sigmas = utils.t_to_alpha_sigma(t)
         if eps is None:
-            eps = self.eps(diffused, t)
+            eps = self.eps(diffused, t, conditioning)
 
         return diffusion_space.decode(
             x * alphas[:, None, None, None]
@@ -128,11 +122,11 @@ class VelocityDiffusion(torch.nn.Module):
             x0 * alphas[:, None, None, None] + noise * sigmas[:, None, None, None]
         )
 
-    def eps(self, diffused, t):
+    def eps(self, diffused, t, conditioning=None):
         x = diffusion_space.encode(diffused)
         if isinstance(t, float) or t.ndim == 0:
             t = torch.full((x.shape[0],), t).to(x)
-        velocity = self.velocity(diffused, t)
+        velocity = self.velocity(diffused, t, conditioning)
         alphas, sigmas = utils.t_to_alpha_sigma(t)
         return x * sigmas[:, None, None, None] + velocity * alphas[:, None, None, None]
 